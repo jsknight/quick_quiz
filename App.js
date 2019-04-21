8ad4db64b3ae87dffdab1b8a4a72cb6425fbc6fd
Jon Knight <jon@metrixapps.com>
2019-04-21 12:07:27 -0700

    Add image and font prefetching and caching

--- App.js
+++ App.js
@@ -6,11 +6,53 @@ import configureStore from "./src/boot/configureStore";
 import Index from "./src/screens/Index";
 import Questions from "./src/screens/Questions";
 import Results from "./src/screens/Results";
+import { AppLoading, Asset, Font } from 'expo';
+import { Ionicons } from "@expo/vector-icons";
 
-const store = configureStore()
+const store = configureStore();
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
 
 export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('./assets/icon.png'),
+    ]);
+
+    const fontAssets = cacheFonts([Ionicons.font]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }
+
+  
   render() {
+
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
     return (
       <Provider store={store}>
         <NativeRouter>
